9b04237bd4262576dead9fd04e3033c269eedbe5
Haakio <matheolecoco9@gmail.com>
2025-05-31 23:44:11 +0200

    Update markClaimed.js

--- api/auth/discord/markClaimed.js
+++ api/auth/discord/markClaimed.js
@@ -1,11 +1,16 @@
+import { connectToDatabase } from '../../../lib/mongodb';
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Méthode non autorisée" });
   }
   const { userId } = req.body;
+  const { db } = await connectToDatabase();
 
-  // Ici tu dois mettre à jour ta base de données pour mettre à 0 ou "claimed"
-  // await markCoinsClaimedInDB(userId);
+  await db.collection('transfers').updateMany(
+    { user_id: userId, done: false },
+    { $set: { done: true } }
+  );
 
   res.status(200).json({ success: true });
 }
